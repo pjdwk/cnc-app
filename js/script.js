94d77ef0220baefb14ea6be31637a603887c45a0
pjdwk <48898167+pjdwk@users.noreply.github.com>
2022-04-10 20:34:06 -0500

    update app

--- js/script.js
+++ js/script.js
@@ -1,158 +1,81 @@
-// START OF PROGRAM VARIABLES
-const button = document.getElementById("submit");
-const roughOdf = document.getElementById("roughOdf");
-const ht = document.getElementById("ht");
-const finishOdf = document.getElementById("finishOdf");
-const odfZstart = document.getElementById("odfZstart");
-const roughZfin = document.getElementById("roughZfin");
-const roughId = document.getElementById("rough-id");
-const container = document.getElementById("container");
+// SELECT SUBMIT BUTTON
+const submitBtn = document.getElementById("submit");
 
-// SELECT 
+// SELECT ROUGH DIMENSIONS
+const roughHt = document.getElementById("r-ht");
+const roughWt = document.getElementById("r-wt");
+const roughOd = document.getElementById("r-od");
+const roughOdf = document.getElementById("r-odf");
+const roughtOal = document.getElementById("r-oal");
+
+// SELECT FINISH DIMENSIONS
+const finishHt = document.getElementById("f-ht");
+const finishWt = document.getElementById("f-wt");
+const finishOd = document.getElementById("f-od");
+const finishOdf = document.getElementById("f-odf");
+const finishtOal = document.getElementById("f-oal");
+
+// SELECT CODE DIMENSIONS
 const codeRoughOdf = document.querySelectorAll(".rough-odf");
-const codeHt = document.querySelectorAll(".hub-t");
-const codeFinishOdf = document.querySelectorAll(".finish-odf");
-const codeOdfZstart = document.querySelectorAll(".z-start-odf");
-const codeRoughOdfAdd = document.querySelectorAll(".rough-odf--add");
-const codeFinOdfAdd = document.querySelectorAll(".fin-odf--add");
-const codeRoughZfin = document.querySelectorAll(".rough-zfin");
-const codeRoughId = document.querySelectorAll(".rough-id");
-const codeRoughIdSub = document.querySelectorAll(".rough-id--sub");
-
-const codeRoughZfinAdd = document.querySelectorAll(".rough-zfin--add");
-const codeDeburr = document.querySelectorAll(".deburr");
 
 
+// PLACEHOLDER VALUES
+let rodfVal, val;
+
+
+
+function removeZeros(arr) {
+    arr = Array.from(arr)
+       while(arr[arr.length -1] == "0") {
+           arr.pop();
+       }
+       arr = arr.join("");
+     }
 
 // EVENT LISTENER
-button.addEventListener("click", function(e){
+submitBtn.addEventListener("click", function(e){
 
-    codeRoughOdf.forEach(function(e,i){
-      e.innerText = parseFloat(roughOdf.value).toFixed(3);
-    });
+    codeRoughOdf.forEach(function(e){
+        // changes value to number with up to 4 decimals
+        rodfVal = parseFloat(roughHt.value).toFixed(4);
+        // converts to string which will allow it to become an array
+        rodfVal = rodfVal.toString();
+        // converts to an array
+        rodfVal = Array.from(rodfVal);
 
-    codeHt.forEach(function(e,i){
-      e.innerText = parseFloat(ht.value).toFixed(3);
-    });
+        // removes the last index of an array if it is a zero
+        while(rodfVal[rodfVal.length -1] == "0") {
+            rodfVal.pop();
+        }
+        
+        // converts value to string
+        rodfVal = rodfVal.join("");
+        // converts back to a number
+        rodfVal = parseFloat(rodfVal);
+        e.innerText = rodfVal;
+    })
 
-    codeFinishOdf.forEach(function(e,i){
-      e.innerText = parseFloat(finishOdf.value).toFixed(3);
-    });
 
-    codeOdfZstart.forEach(function(e,i){
-      e.innerText = parseFloat(odfZstart.value).toFixed(3);
-    });
-  
-    codeRoughOdfAdd.forEach(function(e,i){
-        e.innerText = (parseFloat(roughOdf.value) + .1).toFixed(3);
-    });
 
-    codeFinOdfAdd.forEach(function(e,i){
-      e.innerText = (parseFloat(finishOdf.value) + .1).toFixed(3);
-    });
-  
-    codeRoughZfin.forEach(function(e,i){
-      e.innerText = parseFloat(roughZfin.value).toFixed(3);
-    });
+
     
-  codeRoughIdSub.forEach(function(e,i){
-      e.innerText = (parseFloat(roughId.value) - .6).toFixed(3);
-    });
-  
-  codeRoughId.forEach(function(e,i){
-      e.innerText = parseFloat(roughId.value).toFixed(3);
-    });
-  
-  codeRoughZfinAdd.forEach(function(e,i){
-      e.innerText = (parseFloat(roughZfin.value) + .05).toFixed(3);
-    });
-  
-    codeDeburr.forEach(function(e,i){
-      e.innerText = (parseFloat(finishOdf.value) - .32).toFixed(3);
-    });
-
  })
 
-// const programNumber = document.getElementById("roughOdf"), partNumber = document.getElementById("partNumber"), programVersion = document.getElementById("programVersion"), g54 = document.getElementById("g54"), codeProgramNumber = document.querySelectorAll(".codeProgramNumber"), codePartNumber = document.querySelectorAll(".codePartNumber"), codeProgramVersion = document.querySelectorAll(".codeProgramVersion"), codeG54 = document.querySelectorAll(".codeG54");
-
-// END OF PROGRAM VARIABLES
-
-// let zstart, pipeFaceRoughID, pipeFaceRoughOD, button, variables, codeZstart, codePipeFaceRoughtID, codePipeFaceRoughOD;
-// let finishODP, pipeLength, codeFinishODP, codePipeLength;
-// let finishID, oal, codeFinish, codeOal;
 
 
+//  REMOVES TRAILING ZEROS
+//  let count = '99.1200';
+//  let countArr =  Array.from(count);
+
+//  function removeZero() {
+//    while(countArr[countArr.length -1] == "0") {
+//        countArr.pop();
+//        console.log(countArr);
+//    }
+//    return countArr.join("");
+//  }
 
 
-// PIPE FACE VARIABLES
-// button = document.getElementById("submit");
-// variables = document.querySelectorAll(".var");
-
-// zstart = document.getElementById("zstart");
-// pipeFaceRoughID = document.getElementById("pipeFaceRoughID");  
-// pipeFaceRoughOD = document.getElementById("pipeFaceRoughOD"); 
-
-// codeZstart = document.querySelectorAll(".codeZstart");
-// codePipeFaceRoughID = document.querySelectorAll(".codePipeFaceRoughID");
-// codePipeFaceRoughOD = document.querySelectorAll(".codePipeFaceRoughOD");
-
-// ODP VARIABLES
-// finishODP = document.getElementById("finishODP");
-// pipeLength = document.getElementById("pipeLength");
-// codeFinishODP = document.querySelectorAll(".codeFinishODP");
-// codePipeLength = document.querySelectorAll(".codePipeLength");
-
-// ID VARIABLES
-// finishID = document.getElementById("finishID")
-// oal = document.getElementById("oal")
-// codeFinish = document.querySelectorAll(".codeFinish");
-// codeOal = document.querySelectorAll(".codeOal");
-
-// EVENT LISTENER
-// button.addEventListener("click", function(){
-  
-//   function changeText(code, input) {
-//     for(var i = 0; i < code.length; i++) {
-//       code[i].textContent = input.value;
-//     }
-//   }
- 
-//  // Calculate the base of a triangle
-//  function getTanFromDegrees(degrees) {
-//   return Math.tan(degrees * Math.PI / 180);
-// }
-  
-// function calculateZaxis(side,degree, angleLocation) {
-//   var base;
-  
-//   if(angleLocation === b) {
-//     base = side * getTanFromDegrees(degree);
-//   } 
-  
-//   if(angleLocation === a) {
-//     side = base * getTanFromDegrees(degree);
-//   }
-
-//     return base;
-//   }
-  
-//   // START PROGRAM
-//   changeText(codeProgramNumber, programNumber);
-//   changeText(codePartNumber, partNumber);
-//   changeText(codeProgramVersion, programVersion);
-//   changeText(codeG54, g54);
-  
-//   // PIPE FACE
-//   changeText(codeZstart, zstart);
-//   changeText(codePipeFaceRoughID, pipeFaceRoughID);
-//   changeText(codePipeFaceRoughOD, pipeFaceRoughOD);
-  
-//   // ODP
-//   changeText(codeFinishODP, finishODP);
-//   changeText(codePipeLength, pipeLength);
-  
-//   // ID
-//   changeText(codeFinish, finishID)
-//   changeText(codeOal, oal)
-
-// })
+let num1 = 99.12400;
+num1 = num1.toFixed(4);
+let arr1 = Array.from(num1.toString());
